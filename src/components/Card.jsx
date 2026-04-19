6b8b284e517862a4bc9358ece5364cd8f95d00b3
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-04-19 10:23:46 +0530

    styling card with tailwind 12

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { PenSquareIcon, TrashIcon } from "lucide-react";
 import {Link} from 'react-router-dom';
 function Card({id, title, content, createdAt, onDelete}) {
     return (
-        <div className="flex flex-col justify-between rounded-2xl bg-sky-100 w-screen md:w-auto">
+        <div className="flex flex-col justify-between gap-4 bg-sky-100 w-screen md:w-auto">
             <div className={styles.heading}>
                 <h2 className="bg-gray-300">{title}</h2>
             </div>
