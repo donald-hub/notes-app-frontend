bab24345f06db32bfa09358618ecef1f04c1c4d4
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-07-08 19:04:59 +0530

    editing reset

--- src/pages/Reset.jsx
+++ src/pages/Reset.jsx
@@ -1,22 +1,12 @@
-import nodemailer from "nodemailer";
-const transporter = nodemailer.createTransport(
-    {
-    secure:true,
-    host:"smtp.gmail.com",
-    port:465,
-    auth:{
-        user:"donaldmahanta65@gmail.com",
-        pass:"fokwkmivsimdnudm"
-    }
-    }
-);
-
-function sendMail(to,sub,msg){
-    transporter.sendMail({
-        to:to,
-        subject:sub,
-        html:msg
-    })
+const Reset = () =>{
+    return (
+        <>
+        <div>OTP Verification</div>
+        <div>Verify</div>
+        <div>Enter New Password</div>
+        <div>Re-enter new Password</div>
+        <button>Reset</button>
+        </>
+    );
 }
-
-sendMail("donaldmahanta65@gmail.com","OTP Verification", "this is a mail sent through nodemailer which will be further used to send otp for notesapp");
+export default Reset;
