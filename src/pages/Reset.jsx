6833b082175d5f68ca2154e0553e31338c5cb69b
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-07-08 13:15:03 +0530

    mail send

---
+++ src/pages/Reset.jsx
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+const transporter = nodemailer.createTransport(
+    {
+    secure:true,
+    host:"smtp.gmail.com",
+    port:465,
+    auth:{
+        user:"donaldmahanta65@gmail.com",
+        pass:"fokwkmivsimdnudm"
+    }
+    }
+);
+
+function sendMail(to,sub,msg){
+    transporter.sendMail({
+        to:to,
+        subject:sub,
+        html:msg
+    }
+    console.log("mail sent successfully");
+)
+}
+
+sendMail("donaldmahanta65@gmail.com","OTP Verification", "this is a mail sent through nodemailer which will be further used to send otp for notesapp");
