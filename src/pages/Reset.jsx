877513862c784c22285933a0bac98cff2cc5d58f
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-07-08 19:40:08 +0530

    error fix

--- src/pages/Reset.jsx
+++ src/pages/Reset.jsx
@@ -2,17 +2,16 @@ import crypto from "crypto";
 const Reset = () =>{
 
 
-    function generateOTP() {
-    // Generates a random integer between 100000 (inclusive) and 999999 (inclusive)
-    return crypto.randomInt(100000, 1000000).toString();
-    }
+    // function generateOTP() {
+    // // Generates a random integer between 100000 (inclusive) and 999999 (inclusive)
+    // return crypto.randomInt(100000, 1000000).toString();
+    // }
 
-    console.log("Your generated OTP is", generateOTP());
+    // console.log("Your generated OTP is: ", generateOTP());
 
     return (
         <>
         <h2>OTP Verification</h2>
-        <div>Your generated OTP is </div> 
         <div>Verify</div>
         <div>Enter New Password</div>
         <div>Re-enter new Password</div>
