57b662753e32737f3e83525108b944c46435e366
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-07-08 19:19:15 +0530

    generating otp

--- src/pages/Reset.jsx
+++ src/pages/Reset.jsx
@@ -1,7 +1,17 @@
 const Reset = () =>{
+
+    const crypto = require('crypto');
+
+    function generateOTP() {
+    // Generates a random integer between 100000 (inclusive) and 999999 (inclusive)
+    return crypto.randomInt(100000, 1000000).toString();
+    }
+
+
     return (
         <>
-        <div>OTP Verification</div>
+        <h2>OTP Verification</h2>
+        <div>Your generated OTP is {generateOTP}</div>
         <div>Verify</div>
         <div>Enter New Password</div>
         <div>Re-enter new Password</div>
