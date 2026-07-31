b9ac309b93240f1f48e2030b51282573417f5d18
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-07-31 12:36:38 +0530

    styling addNew page

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -2,13 +2,8 @@ import {Link} from 'react-router';
 import styles from './AddNew.module.css';
 
 function AddNew() {
-    return 
-    <>
-    <div className="bg-gray-300">
-        <div className={styles.addNote}>
+    return <div className={styles.addNote}>
         <Link to="/create" className={styles.addButton}>+</Link>
         </div>
-    </div>
-    </>
 }
 export default AddNew
