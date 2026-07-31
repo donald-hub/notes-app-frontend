9ef0d578ecfd354a1bce108c61bf4b77ade95db4
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-07-31 11:42:19 +0530

    editing note card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,14 @@ import { PenSquareIcon, TrashIcon } from "lucide-react";
 import {Link} from 'react-router-dom';
 function Card({id, title, content, createdAt, onDelete}) {
     return (
-        <div className={styles.card}>
+        <div className={`${styles.card} bg-gray-300`}>
             <div className={styles.heading}>
                 <h2 className="bg-gray-300">{title}</h2>
             </div>
-            <div className={`${styles.content} bg-gray-300`}>
+            <div className={styles.content}>
                 <p>{content}</p>
             </div>
-            <div className={`${styles.footer} bg-gray-300`}>
+            <div className={styles.footer}>
             <span className={styles.date}>{formatDate(createdAt)}</span>
             <span className={styles.actions}>
                 <Link to={`/update/${id}`}><PenSquareIcon className={styles.edit}/></Link>
