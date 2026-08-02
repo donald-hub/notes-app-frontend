1cbe88708c862cdb663a7cf69e2c91e49cbec081
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-08-02 20:55:12 +0530

    styling addnew  button

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -4,7 +4,7 @@ import { IoAddCircle } from "react-icons/io5";
 
 function AddNew() {
     return <div className={styles.addNote}>
-        <Link to="/create" className={styles.addButton}><IoAddCircle color="honeydew" size="50"/></Link>
+        <Link to="/create" className={styles.addButton}><IoAddCircle color="yellow" size="50"/></Link>
         </div>
 }
 export default AddNew
