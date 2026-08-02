d74a65663ab5fe4e7aed27b7173e0656fc610bf7
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-08-02 20:06:41 +0530

    styling addnew  page

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -4,7 +4,7 @@ import {IoIosAdd} from 'react-icons/io';
 
 function AddNew() {
     return <div className={styles.addNote}>
-        <IoIosAdd size="5" />
+        <IoIosAdd size="50" />
         </div>
 }
 export default AddNew
