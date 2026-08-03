abf4841de4e79ce6b3b2c5a93de2f6084201f8db
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-08-03 10:06:58 +0530

    editing separate cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,17 +6,17 @@ function Card({id, title, content, createdAt, onDelete}) {
     return (
         <div className={`${styles.card} bg-gray-300`}>
             <div className={styles.heading}>
-                <h2 className="bg-gray-300">{title}</h2>
+                <h2 className="bg-gray-300">{title}</h2><span className={styles.actions}>
+                <Link to={`/update/${id}`}><PenSquareIcon className={styles.edit}/></Link>
+                <TrashIcon className={styles.delete} onClick={() => onDelete(id)}/>
+            </span>
             </div>
             <div className={styles.content}>
                 <p>{content}</p>
             </div>
             <div className={styles.footer}>
             <span className={styles.date}>{formatDate(createdAt)}</span>
-            <span className={styles.actions}>
-                <Link to={`/update/${id}`}><PenSquareIcon className={styles.edit}/></Link>
-                <TrashIcon className={styles.delete} onClick={() => onDelete(id)}/>
-            </span>
+            
             </div>
         </div>
     );
