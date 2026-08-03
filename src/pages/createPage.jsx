a4f4d779b60f88accf6adcdade319763c5e7358b
donald-hub <56503357+donald-hub@users.noreply.github.com>
2026-08-03 10:39:15 +0530

    go to notes

--- src/pages/createPage.jsx
+++ src/pages/createPage.jsx
@@ -44,7 +44,7 @@ const createPage = () => {
     <Navbar />
     <div className={styles.authPage}>
     <div className={styles.header}>
-      <Link to="/" ><ArrowLeftIcon className={styles.backIcon}/>
+      <Link className="flex" to="/" ><ArrowLeftIcon className={styles.backIcon}/>
       <span className={styles.backTitle}>Go to Notes</span></Link>
     </div>
     <form className={styles.form} onSubmit={handleSubmit}>
